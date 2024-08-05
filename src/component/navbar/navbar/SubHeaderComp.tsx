634c5e84076ef7ba72e487dee96b5363930db497
umoren <umorenim@gmail.com>
2024-08-05 09:28:56 -0700

    updated

--- src/component/navbar/navbar/SubHeaderComp.tsx
+++ src/component/navbar/navbar/SubHeaderComp.tsx
@@ -29,26 +29,6 @@ export const SubHeaderComp = () => {
           <Latcard />
         </div>
       </div>
-      <div className="flex justify-center items-center bg-black text-white p-4 overflow-y-auto">
-        <div className="w-full max-w-sm min-h-screen h-auto mt-20">
-          <div className=" py-2 flex justify-around items-center">
-            <button className="bg-slate-600 px-2 rounded-md text-sm">
-              Non-vip
-            </button>
-            <button className="bg-slate-600 px-2 rounded-md text-sm">
-              Main Account
-            </button>
-            <button className="bg-slate-600 px-4 rounded-md  flex justify-center items-center gap-1 text-sm">
-              <RiVerifiedBadgeFill />
-              Verified ID
-            </button>
-          </div>
-          <SubCard />
-          <RowCard />
-          <Reward />
-          <Latcard />
-        </div>
-      </div>
     </>
   );
 };
